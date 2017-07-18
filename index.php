8b59e98959b23f40b8a3a80a279730c48b9c59bc
MewX <imewx@qq.com>
2017-07-18 21:16:45 +0930

    Fixed #53: added index page for web browsing

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MewX
+ * Date: 7/18/2017
+ * Time: 09:15 PM
+ */
+
+// goto home page
+header("Location: student/welcome.php"); // ==> ../index.php
+exit;
