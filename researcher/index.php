de255279f6aa9716dd64d9bdfd9fecfbde8e20e5
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-07-22 22:23:22 +0930

    remove

--- researcher/index.php
+++ researcher/index.php
@@ -4,7 +4,7 @@ require_once("../mysql-lib.php");
 require_once("../debug.php");
 require_once("researcher-lib.php");
 $conn = db_connect();
-$connLog = db_connect('log');
+//$connLog = db_connect('log');
 
 try {
     $bugNum = getUnsolvedLogNum($connLog);
