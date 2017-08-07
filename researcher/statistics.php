36b0ea1b5578f785a64c9cb73333869189d533aa
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-08-07 15:30:35 +0930

    edit header title

--- researcher/statistics.php
+++ researcher/statistics.php
@@ -75,7 +75,7 @@ db_close($conn);
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Statistic Overview</h1>
+                <h1 class="page-header">Statistics Overview</h1>
             </div>
             <!-- /.col-lg-12 -->
         </div>
