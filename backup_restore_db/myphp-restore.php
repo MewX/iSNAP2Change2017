31cc5d41bec7d29408cf52270d6caee2156e2573
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-10-09 00:21:58 +1030

    update message table

--- backup_restore_db/myphp-restore.php
+++ backup_restore_db/myphp-restore.php
@@ -4,7 +4,7 @@ $uploadfile = basename($_FILES['userfile']['name']);
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 //the path here should be the full path for mysqldump
-    $cmd = "/usr/local/bin/mysql -u$username -p$password $database < $uploadfile";
+    $cmd = "mysql -u$username -p$password $database < $uploadfile";
     //$cmd = "/usr/local/bin/mysql --user $username --password= $password $database < $uploadfile";
     system($cmd);
     echo "Your database is restore successfully";
