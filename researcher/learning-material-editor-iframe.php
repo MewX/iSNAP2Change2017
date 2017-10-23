fec4a31c417fa43b3f2eaaf944934fa929ad97ea
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-10-23 21:06:41 +1030

    add Notification for materials editor

--- researcher/learning-material-editor-iframe.php
+++ researcher/learning-material-editor-iframe.php
@@ -13,5 +13,21 @@
                 style="width: 100%; height:100%;"></iframe>
     </div>
     <!-- /.panel-body -->
+
+    <div class="panel-footer">
+        <h4><?php echo $pageNameForView; ?> Notification</h4>
+        <div class="alert alert-info">
+            <p>You need to click the save button to save your changes in the editor</p>
+        </div>
+        <div class="alert alert-info">
+            <p>If you want to upload a picture on your computer to this editor, please:<br>
+                1. Upload your picture to https://imgur.com/upload<br>
+                2. Click get share link, copy the BBCode (Forums)<br>
+                3. In our editor, click the image icon, paste this link in to Source text box.<br>
+                4. Remove[img] and [/img/ before/after this link<br>
+                5. click ok<br>
+            </p>
+        </div>
+    </div>
 </div>
 <!-- /.panel -->
