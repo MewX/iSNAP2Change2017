d6e226e7404f88537796b2ea0d448f4794ae5737
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-11-01 11:14:06 +1030

    poster grader

--- researcher/poster-grader.php
+++ researcher/poster-grader.php
@@ -51,7 +51,7 @@ db_close($conn);
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Grader</h1>
+                <h1 class="page-header">Poster Grader</h1>
             </div>
             <!-- /.col-lg-12 -->
         </div>
