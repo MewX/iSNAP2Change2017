5678c43e1aa1be04e8fbb923d7ed1fa2e1617819
TaoZhang5023 <a1680974@student.adelaide.edu.au>
2017-11-28 15:57:24 +1030

    remove plus icon in dashboard.php

--- researcher/dashboard.php
+++ researcher/dashboard.php
@@ -12,8 +12,7 @@
             <div class="col-lg-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Dashboard <span class="glyphicon glyphicon-plus pull-right" data-toggle="modal"
-                                        data-target="#dialog"></span>
+                        Dashboard
                     </div>
                     <!-- /.panel-heading -->
                     <div class="panel-body">
